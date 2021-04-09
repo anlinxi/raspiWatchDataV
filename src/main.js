59707a48d7a1486edcad544fe81ebeae0cf51409
anlinxi <anlinxi@gmail.com>
2021-04-09 17:13:48 +0800

    增加国际化语言模块

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueI18n from 'vue-i18n'
+import i18n from './locales'
 
 import './assets/common.less'
 
@@ -8,7 +10,9 @@ import dataV from '@jiaminghi/data-view'
 Vue.config.productionTip = false
 
 Vue.use(dataV)
+Vue.use(VueI18n)
 
 new Vue({
+  i18n,
   render: h => h(App)
 }).$mount('#app')
